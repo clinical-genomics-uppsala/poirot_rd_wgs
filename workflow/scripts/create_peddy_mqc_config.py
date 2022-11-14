ae00037a86780c3fa14448994cf593c6d5567914
Pádraic Corcoran <padco251@marvin-head.cgu10.igp.uu.se>
2022-11-14 14:12:23 +0100

    Update peddy scripts

--- workflow/scripts/create_peddy_mqc_config.py
+++ workflow/scripts/create_peddy_mqc_config.py
@@ -12,12 +12,7 @@ def comment_the_config_keys(config_dict):
 
     return commented_config
 
-
 def get_trio_info(ped_filepath):
-    '''
-
-
-    '''
 
     ped_cols = ['family_id', 'sample_id', 'paternal_id',
     'maternal_id', 'sex', 'phenotype']
@@ -28,66 +23,35 @@ def get_trio_info(ped_filepath):
     for row in ped_df.itertuples():
         trio_membership_dict[row.sample_id] = row.family_id
 
-    return (ped_df, trio_membership_dict)
+    return trio_membership_dict
 
 
-
-# def get_relatedness_df(peddy_rel_file_path,ped_df, trio_samples):
-#     relatedness_df = pd.read_csv(peddy_rel_file_path)
-#
-#     relatedness_df["rel_check_test"] = np.where(
-#     relatedness_df.pedigree_parents ==  relatedness_df.predicted_parents,
-#      'Pass', 'Fail') # report peddy  error check as pass/fail for simplicity
-#
-#     #subset df to only trio samples
-#     trio_relatedness_df = relatedness_df[
-#     relatedness_df.sample_a.isin(trio_samples)
-#     & relatedness_df.sample_b.isin(trio_samples) ]
-#
-#     trio_relatedness_with_id_df = pd.merge(
-#     ped_df[['family_id', 'sample_id']],trio_relatedness_df,how='right',
-#     left_on='sample_id', right_on='sample_a')
-#
-#     trio_relatedness_with_id_df.drop('sample_id', axis=1, inplace=True)
-#
-#     trio_relatedness_with_id_df['Sample_pair'] = trio_relatedness_with_id_df[
-#     ['sample_a', 'sample_b']].agg('_v_'.join, axis=1)
-#
-#     trio_relatedness_with_id_df = trio_relatedness_with_id_df[
-#     sorted(trio_relatedness_with_id_df.columns)]
-#
-#     return trio_relatedness_with_id_df
-
-def get_relatedness_df(peddy_rel_file_path,ped_df, trio_membership_dict):
+def get_relatedness_df(peddy_rel_file_path, trio_dict):
     relatedness_df = pd.read_csv(peddy_rel_file_path)
 
-    relatedness_df["rel_check_test"] = np.where(
-    relatedness_df.pedigree_parents ==  relatedness_df.predicted_parents,
-     'Pass', 'Fail') # report peddy  error check as pass/fail for simplicity
+    relatedness_df["rel_check_test"] = np.where((relatedness_df.parent_error == True) | 
+    (relatedness_df.sample_duplication_error == True), 'Fail', 'Pass') 
 
-    #subset df to only trio samples
-    relatedness_with_sample_a_id = pd.merge(
-    ped_df[['family_id', 'sample_id']],relatedness_df,how='right',
-    left_on='sample_id', right_on='sample_a')
+    trio_idx = []
+    non_trio_idx = [] 
+    for row in relatedness_df.itertuples():
+        if trio_dict[row.sample_a] == trio_dict[row.sample_b]:
+            trio_idx.append(row.Index) # get the indices of trio pairs
+        else:
+            non_trio_idx.append(row.Index)
 
-    # check that sample_a and sample_b are in the same trio
-    trio_pair_indices = []
-    for sample_pair in relatedness_with_sample_a_id.itertuples():
-        sample_a_trio = trio_membership_dict[sample_pair.sample_a]
-        sample_b_trio = trio_membership_dict[sample_pair.sample_b]
-        if sample_a_trio == sample_b_trio:
-            trio_pair_indices.append(sample_pair.Index)
+    if len(trio_idx) > 0: # check if there are any trios
+        trio_rel_df = relatedness_df.iloc[trio_idx, :]
+        non_trio_rel_df = relatedness_df.iloc[non_trio_idx , :]
+        
+        error_rel_df = non_trio_rel_df[(non_trio_rel_df['parent_error'] == True) | 
+            (non_trio_rel_df['sample_duplication_error'] == True)] 
+        
+        rel_df = pd.concat([trio_rel_df, error_rel_df ])
+    else:
+        rel_df = relatedness_df[(relatedness_df['parent_error'] == True) | (relatedness_df['sample_duplication_error'] == True)] 
 
-    trio_pairs_df = relatedness_with_sample_a_id.iloc[trio_pair_indices].sort_values(by='family_id')
-
-    trio_pairs_df.drop('sample_id', axis=1, inplace=True)
-
-    trio_pairs_df['Sample_pair'] = trio_pairs_df[
-                                ['sample_a', 'sample_b']].agg('_v_'.join, axis=1)
-
-    trio_pairs_sorted_df = trio_pairs_df[sorted(trio_pairs_df.columns)]
-
-    return trio_pairs_sorted_df
+    return rel_df 
 
 
 def get_sex_check_df(sex_check_file_path):
@@ -106,27 +70,42 @@ def write_peddy_mqc(peddy_df, peddy_config, outfile):
 
         peddy_df.to_csv(outfile, sep='\t', mode='a', index=False)
 
+def get_trio_id(sample_id, trio_dict):
+
+    trio_id = trio_dict[sample_id]
+    if trio_id == sample_id:
+        trio_id = 'NA'
+    return trio_id
+
 
 def main():
 
     try:
-        config = snakemake.config.get("peddy", '').get("config", '')
-
+        
+        # config = snakemake.config.get("peddy", '').get("config", '')
+        config = '../../config/peddy_mqc.yaml'
 
         with open(config, 'r') as report_configs:
             peddy_mqc_configs = yaml.load(report_configs, Loader=yaml.FullLoader)
 
-            ped_file = snakemake.input.ped
-            ped_df, trio_dict = get_trio_info(ped_file)
+            # ped_file = snakemake.input.ped
+            ped_file = 'all.ped'
+            trio_dict = get_trio_info(ped_file)
 
-            rel_check_df = get_relatedness_df(snakemake.input.peddy_rel_check,
-                                                ped_df, trio_dict)
+            # rel_check_df = get_relatedness_df(snakemake.input.peddy_rel_check, trio_dict)
+            rel_check_df = get_relatedness_df('peddy.ped_check.csv', trio_dict)
+
+            rel_check_df['trio_id'] = rel_check_df['sample_a'].apply(get_trio_id, args=(trio_dict,))
+            rel_check_df.sort_values(by=['trio_id'], inplace=True)
             peddy_rel_config = peddy_mqc_configs.get('peddy_rel_check')
-            write_peddy_mqc(rel_check_df, peddy_rel_config, snakemake.output.rel_check_mqc)
+            # write_peddy_mqc(rel_check_df, peddy_rel_config, snakemake.output.rel_check_mqc)
+            write_peddy_mqc(rel_check_df, peddy_rel_config, 'test_trio.txt')
 
-            sex_check_df = get_sex_check_df(snakemake.input.peddy_sex_check)
+            # sex_check_df = get_sex_check_df(snakemake.input.peddy_sex_check)
+            sex_check_df = get_sex_check_df('peddy.sex_check.csv')
             peddy_sex_config = peddy_mqc_configs.get('peddy_sex_check')
-            write_peddy_mqc(sex_check_df, peddy_sex_config, snakemake.output.sex_check_mqc)
+            write_peddy_mqc(sex_check_df, peddy_sex_config, 'test_sex.txt')
+            # write_peddy_mqc(sex_check_df, peddy_sex_config, snakemake.output.sex_check_mqc)
 
     except FileNotFoundError:
         sys.exit('Path to peddy config file not found/specified in config.yaml')
