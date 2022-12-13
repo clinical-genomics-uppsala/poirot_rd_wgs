d9344d38bd01c96122cc5409e7f53922d4a5f119
Arielle R Munters <arielle@reivant.se>
2022-12-13 14:18:40 +0100

    perf: fix sample order so it works for up to 999 samples

--- workflow/scripts/sample_order_for_multiqc.py
+++ workflow/scripts/sample_order_for_multiqc.py
@@ -19,7 +19,7 @@ with open(snakemake.output.replacement, "w+") as tsv:
     tsv_writer = csv.writer(tsv, delimiter='\t')
     i = 1
     for sample in samples:
-        tsv_writer.writerow([sample, 'sample'+str(i)])
+        tsv_writer.writerow([sample, 'sample_'+str(f"{i:03}")])
         i += 1
 
 with open(snakemake.output.order, "w+") as tsv:
@@ -27,5 +27,5 @@ with open(snakemake.output.order, "w+") as tsv:
     tsv_writer.writerow(["Sample Order", "Sample Name"])
     i = 1
     for sample in samples:
-        tsv_writer.writerow(['sample'+str(i), sample])
+        tsv_writer.writerow(['sample_'+str(f"{i:03}"), sample])
         i += 1
