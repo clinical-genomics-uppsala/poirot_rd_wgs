f55adb63446f9b900f897354bd8201abbfdfa68b
Pádraic Corcoran <padraicc@users.noreply.github.com>
2023-08-24 08:57:12 +0200

    fix: Update cnvpytor_vcf_to_aed.py

--- workflow/scripts/cnvpytor_vcf_to_aed.py
+++ workflow/scripts/cnvpytor_vcf_to_aed.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 
-from pysam import VariantFile
-
 vcf_in = snakemake.input.vcf
 aed = snakemake.output.aed
 
-vcf = VariantFile(vcf_in)
+def get_info_dict(info_list):
+    info_dict = {}
+    for i in info_list:
+        info_rec = i.split("=")
+        if info_rec[0] == 'IMPRECISE':
+            continue
+        info_dict[info_rec[0]] = info_rec[1]
+
+    return info_dict
+
 
 header_0 = ["bio:sequence(aed:String)", "bio:start(aed:Integer)",
             "bio:end(aed:Integer)", "aed:name(aed:String)",
-            "aed:value(aed:String)", "aed:category(aed:String)",
+            "aed:value(aed:String)","aed:category(aed:String)",
             "style:color(aed:Color)"]
 
 header_1 = ["", "", "", "namespace:affx(aed:URI)",
@@ -22,15 +29,24 @@ with open(aed, 'w') as outfile:
     print('\t'.join(header_0), file=outfile)
     print('\t'.join(header_1), file=outfile)
     print('\t'.join(header_2), file=outfile)
-    for rec in vcf:
-        var_type = rec.info["SVTYPE"]
-        if var_type == 'DEL':
-            aed_vartype = "copynumber/loss"
-            colour = "rgb(255,0,0)"
-        elif var_type == 'DUP':
-            aed_vartype = "copynumber/gain"
-            colour = "rgb(0,0,255)"
+    with open(vcf_in, 'r') as vcf:
+        for rec in vcf:
+            if rec.startswith('#'):
+                continue
+            vcf_rec = rec.split('\t')
+            contig = vcf_rec[0]
+            start = int(vcf_rec[1]) - 1
+            rec_id = vcf_rec[2]
+            info_list = vcf_rec[7].split(";")
+            info_dict = get_info_dict(info_list)
+            var_type = info_dict["SVTYPE"]
+            end = info_dict["END"]
+            if var_type == 'DEL':
+                aed_vartype = "copynumber/loss"
+                colour = "rgb(255,0,0)"
+            elif var_type == 'DUP':
+                aed_vartype = "copynumber/gain"
+                colour = "rgb(0,0,255)"
 
-        print(rec.contig, rec.start,
-              rec.stop, rec.id, "", aed_vartype,
-              colour, sep="\t", file=outfile)
+            print(contig, start, end, rec_id, "", aed_vartype, colour,
+                  sep="\t", file=outfile)
