154e140457464fc9ceac243e6c86611913928c85
zezzipa <jessika_90@hotmail.com>
2023-08-30 14:08:41 +0200

    fix: update and rename start_Poirot_marvin.sh to start_HG_marvin.sh

---
+++ start_HG_marvin.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+# To run script:
+# bash /projects/wp3/nobackup/TWIST/Bin/Poirot/start_TE_marvin.sh /projects/wp3/nobackup/WGS/*/230619_NDX550925_RUO_0133_AHJ2YMBGXT/Sample_* HG4
+
+module load slurm-drmaa/1.1.3
+module load snakemake/7.22.0
+module load singularity/3.7.1
+source /beegfs-storage/projects/wp3/nobackup/TWIST/Bin/Poirot/hydra-env/bin/activate;
+pip install -r /beegfs-storage/projects/wp3/nobackup/TWIST/Bin/Poirot/requirements.txt
+
+fastqFolder=$1
+sequencerun=$2    #Sequence ID
+startDir=$PWD
+
+poirotFolder=/beegfs-storage/projects/wp3/nobackup/TWIST/Bin/Poirot
+outbox=$(echo $(echo ${poirotFolder} | rev | cut -d/ -f3- | rev)/OUTBOX)
+
+# Create outbox and scratch folders
+echo 'Creating outbox and scratch folders'
+if [ ! -d "/scratch/wp3/TWIST/${sequencerun}/" ]
+then
+  mkdir /scratch/wp3/TWIST/${sequencerun}/
+fi
+
+if [ ! -d "${outbox}/${sequencerun}/" ]
+then
+  mkdir ${outbox}/${sequencerun}/
+fi
+
+# Cp data to scratch
+echo 'Copy to scratch' && \
+#To save space on scratch I comment out copy fastq-files there
+#rsync -ru ${fastqFolder%}/Sample_WD* /scratch/wp3/TWIST/${sequencerun}/fastq  && \
+rsync -ru SampleSheet.csv /scratch/wp3/TWIST/${sequencerun}/  && \
+rsync -ru ${poirotFolder}/config /scratch/wp3/TWIST/${sequencerun}/  && \
+
+cd /scratch/wp3/TWIST/${sequencerun}/  && \
+
+# Create sample and unit files
+#To save space on scratch we leave the fastq-files in nobackup
+#hydra-genetics create-input-files -d /scratch/wp3/TWIST/${sequencerun} -t N --tc 0 -f && \
+hydra-genetics create-input-files -d ${fastqFolder%} -t N --tc 0 -f && \
+
+# Get trio information from SampleSheet.csv into config/samples.tsv
+python ${poirotFolder}/extract_sample_sheet_info.py SampleSheet.csv && \
+
+# Start pipeline
+snakemake --profile ${poirotFolder}/profiles/slurm/ -s ${poirotFolder}/workflow/Snakefile \
+--configfile config/config.yaml -p --latency-wait 5 --rerun-incomplete --keep-going && \
+
+#Copy data back
+echo 'Pipeline done and cp back data to OUTBOX' && \
+cp /scratch/wp3/TWIST/${sequencerun}/results/multiqc_DNA.html /scratch/wp3/TWIST/${sequencerun}/results/multiqc_${sequencerun}.html
+rsync -ru /scratch/wp3/TWIST/${sequencerun}/results/* ${outbox}/${sequencerun}/ && \
+rsync -ru /scratch/wp3/TWIST/${sequencerun}/.snakemake ${startDir}/ && \
+echo 'All done!'
