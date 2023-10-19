3af804554e6fbef4465e20885fb4ed9859b4ea5c
Padraic Corcoran <padraic.corcoran@scilifelab.uu.se>
2023-10-19 10:31:01 +0200

    feat: run cnpytor with rd_mean_shift and combined models

--- run_poirot.sh
+++ run_poirot.sh
@@ -5,13 +5,15 @@ set -u
 set -o pipefail
 
 # source virtual env with requirements.txt installed
-source ~/hydra-env/bin/activate 
+python3.9 -m venv hydra_env
+source hydra_env/bin/activate
+pip install -r requirements.txt 
 
 module load singularity/3.7.1
 module load slurm-drmaa/1.1.3 
 
-sample_sheet=/beegfs-storage/projects/wp3/nobackup/WGS/WGS_pilot_CGU202111/THG11/230330_A00181_0643_AHNN37DSX5/SampleSheet.csv
-fastq_dir=/beegfs-storage/projects/wp3/nobackup/WGS/WGS_pilot_CGU202111/THG11/230330_A00181_0643_AHNN37DSX5/
+sample_sheet=/beegfs-storage/projects/wp4/nobackup/DDS_downloads/DataDelivery_2023-08-22_16-10-05_snpseq00449/files/WD-3649/230818_A00181_0673_AHKCL2DSX7/SampleSheet.csv
+fastq_dir=/beegfs-storage/projects/wp4/nobackup/DDS_downloads/DataDelivery_2023-08-22_16-10-05_snpseq00449/files/WD-3649/230818_A00181_0673_AHKCL2DSX7/
 
 ## barcodes not currently in the Nextseq fastq headers, so use Ns 
 hydra-genetics create-input-files -d ${fastq_dir} -t N --tc 0  -f
@@ -19,4 +21,4 @@ hydra-genetics create-input-files -d ${fastq_dir} -t N --tc 0  -f
 # adds trio info to samples.tsv and renames to SampleSheet.csv ids, create sample order files for multiqc
 python extract_sample_sheet_info.py ${sample_sheet} 
 
-snakemake  --profile profiles/slurm/ --configfile config/config.yaml -pn
+snakemake  --profile profiles/slurm/ --configfile config/config.yaml -p
