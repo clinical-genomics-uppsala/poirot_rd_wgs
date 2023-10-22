af55a2aabc80d9422fa01447868f9a906a224611
Padraic Corcoran <padraic.corcoran@scilifelab.uu.se>
2023-10-22 17:03:25 +0200

    feat: add singularity download and reference files compression

--- build_conda.sh
+++ build_conda.sh
@@ -1,50 +1,55 @@
 #!/usr/bin/env bash
 eval "$(conda shell.bash hook)"
 
-POIROT_VERSION=develop
+POIROT_VERSION=bianca_support # version or branch
+reference_files=reference_files/ # path to reference files
 
-conda create --name poirot_rd_wgs_${POIROT_VERSION} python=3.9 -y
+conda create --name poirot_env python=3.9 -y
 
-conda activate poirot_rd_wgs_${POIROT_VERSION}
+conda activate poirot_env
 
 conda install -c conda-forge pip -y
 
-if [ -d poirot_rd_wgs_${POIROT_VERSION} ];
+if [ -d poirot_pipeline ];
 then
-    rm -fr poirot_rd_wgs_${POIROT_VERSION}
+    rm -fr poirot_pipeline
 fi
 
-mkdir poirot_rd_wgs_${POIROT_VERSION}
+mkdir poirot_pipeline
 
-git clone --branch ${POIROT_VERSION} https://github.com/clinical-genomics-uppsala/poirot_rd_wgs poirot_rd_wgs_${POIROT_VERSION}/poirot_rd_wgs
+tar -czvf poirot_pipeline/reference_files.tar.gz ${reference_files}
 
-pip install -r poirot_rd_wgs_${POIROT_VERSION}/poirot_rd_wgs/requirements.txt 
+git clone --branch ${POIROT_VERSION} https://github.com/clinical-genomics-uppsala/poirot_rd_wgs poirot_pipeline/poirot_rd_wgs_${POIROT_VERSION}
+
+pip install -r poirot_pipeline/poirot_rd_wgs_${POIROT_VERSION}/requirements.txt 
+
+# pull the singlarity containers
+hydra-genetics prepare-environment create-singularity-files -c config/config.yaml -o poirot_pipeline/singularity_files
 
 conda deactivate
 
-conda-pack -n poirot_rd_wgs_${POIROT_VERSION} -o poirot_rd_wgs_${POIROT_VERSION}/env.tar.gz
+conda-pack -n poirot_env -o poirot_pipeline/poirot_env.tar.gz
 
-mkdir -p poirot_rd_wgs_${POIROT_VERSION}/hydra-genetics
+mkdir -p poirot_pipeline/hydra-genetics
 
-git clone https://github.com/snakemake/snakemake-wrappers.git poirot_rd_wgs_${POIROT_VERSION}/snakemake-wrappers
+git clone https://github.com/snakemake/snakemake-wrappers.git poirot_pipeline/snakemake-wrappers
+
+git clone https://github.com/hydra-genetics/alignment.git poirot_pipeline/hydra-genetics/alignment
+git clone https://github.com/hydra-genetics/annotation.git poirot_pipeline/hydra-genetics/annotation
+git clone https://github.com/hydra-genetics/cnv_sv.git poirot_pipeline/hydra-genetics/cnv_sv
+git clone https://github.com/hydra-genetics/compression.git poirot_pipeline/hydra-genetics/compression
+git clone https://github.com/hydra-genetics/filtering.git poirot_pipeline/hydra-genetics/filtering
+git clone https://github.com/hydra-genetics/misc.git poirot_pipeline/hydra-genetics/misc
+git clone https://github.com/hydra-genetics/mitochondrial.git poirot_pipeline/hydra-genetics/mitochondrial
+git clone https://github.com/hydra-genetics/prealignment.git poirot_pipeline/hydra-genetics/prealignment
+git clone https://github.com/hydra-genetics/parabricks.git poirot_pipeline/hydra-genetics/parabricks
+git clone https://github.com/hydra-genetics/qc.git poirot_pipeline/hydra-genetics/qc
+git clone https://github.com/hydra-genetics/snv_indels.git poirot_pipeline/hydra-genetics/snv_indels
 
 
-git clone https://github.com/hydra-genetics/alignment.git poirot_rd_wgs_${POIROT_VERSION}/hydra-genetics/alignment
-git clone https://github.com/hydra-genetics/annotation.git poirot_rd_wgs_${POIROT_VERSION}/hydra-genetics/annotation
-git clone https://github.com/hydra-genetics/cnv_sv.git poirot_rd_wgs_${POIROT_VERSION}/hydra-genetics/cnv_sv
-git clone https://github.com/hydra-genetics/compression.git poirot_rd_wgs_${POIROT_VERSION}/hydra-genetics/compression
-git clone https://github.com/hydra-genetics/filtering.git poirot_rd_wgs_${POIROT_VERSION}/hydra-genetics/filtering
-git clone https://github.com/hydra-genetics/misc.git poirot_rd_wgs_${POIROT_VERSION}/hydra-genetics/misc
-git clone https://github.com/hydra-genetics/mitochondrial.git poirot_rd_wgs_${POIROT_VERSION}/hydra-genetics/mitochondrial
-git clone https://github.com/hydra-genetics/prealignment.git poirot_rd_wgs_${POIROT_VERSION}/hydra-genetics/prealignment
-git clone https://github.com/hydra-genetics/parabricks.git poirot_rd_wgs_${POIROT_VERSION}/hydra-genetics/parabricks
-git clone https://github.com/hydra-genetics/qc.git poirot_rd_wgs_${POIROT_VERSION}/hydra-genetics/qc
-git clone https://github.com/hydra-genetics/snv_indels.git poirot_rd_wgs_${POIROT_VERSION}/hydra-genetics/snv_indels
+tar -zcvf poirot_pipeline.tar.gz poirot_pipeline
 
-
-tar -zcvf poirot_rd_wgs_${POIROT_VERSION}.tar.gz poirot_rd_wgs_${POIROT_VERSION}
-
-if [ -d poirot_rd_wgs_${POIROT_VERSION} ];
+if [ -d poirot_pipeline ];
 then
-    rm -fr poirot_rd_wgs_${POIROT_VERSION}
+    rm -fr poirot_pipeline
 fi
