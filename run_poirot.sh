7af648d7bf6ba5e073058015907f4b432eac89cb
Padraic Corcoran <padraic.corcoran@scilifelab.uu.se>
2023-11-27 15:50:29 +0100

    change to writing to a results to a separate directory

--- run_poirot.sh
+++ run_poirot.sh
@@ -4,6 +4,13 @@ set -e
 set -u
 set -o pipefail
 
+output=/beegfs-storage/projects/wp3/nobackup/WGS/NovaSeqX_validation_runs/20231030_LH00338_0003_A22FJ27LT3/poirot_output
+
+if [ ! -d ${output} ]; then
+    mkdir ${output}
+    mkdir ${output}/slurm
+fi
+
 # source virtual env with requirements.txt installed
 python3.9 -m venv hydra_env
 source hydra_env/bin/activate
@@ -12,8 +19,8 @@ pip install -r requirements.txt
 module load singularity/3.7.1
 module load slurm-drmaa/1.1.3 
 
-sample_sheet=/beegfs-storage/projects/wp4/nobackup/DDS_downloads/DataDelivery_2023-08-22_16-10-05_snpseq00449/files/WD-3649/230818_A00181_0673_AHKCL2DSX7/SampleSheet.csv
-fastq_dir=/beegfs-storage/projects/wp4/nobackup/DDS_downloads/DataDelivery_2023-08-22_16-10-05_snpseq00449/files/WD-3649/230818_A00181_0673_AHKCL2DSX7/
+sample_sheet=/beegfs-storage/projects/wp3/nobackup/WGS/NovaSeqX_validation_runs/20231030_LH00338_0003_A22FJ27LT3/SampleSheet.csv
+fastq_dir=/beegfs-storage/projects/wp3/nobackup/WGS/NovaSeqX_validation_runs/20231030_LH00338_0003_A22FJ27LT3/fastq/
 
 ## barcodes not currently in the Nextseq fastq headers, so use Ns 
 hydra-genetics create-input-files -d ${fastq_dir} -t N --tc 0  -f
@@ -21,4 +28,6 @@ hydra-genetics create-input-files -d ${fastq_dir} -t N --tc 0  -f
 # adds trio info to samples.tsv and renames to SampleSheet.csv ids, create sample order files for multiqc
 python extract_sample_sheet_info.py ${sample_sheet} 
 
-snakemake  --profile profiles/slurm/ --configfile config/config.yaml -p
+rsync -ruv config ${output}
+
+snakemake  --profile profiles/slurm/ --configfile config/config.yaml -p  -d ${output} --notemp
